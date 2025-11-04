00502cd34720b1883988f0442a9b720e58e4d05b
Lee-CH810 <2002ckdghks@naver.com>
2025-11-04 21:04:28 +0900

    [FEAT] Memory 구조체를 클래스로 변경

--- TASK9/task9-2.cpp
+++ TASK9/task9-2.cpp
@@ -46,12 +46,38 @@ string hexstrToBin(char hexChar) {
     return bitset<4>(value).to_string();
 }
 
-struct Memory {
+/**
+ * Memory 클래스
+ */
+class Memory {
+private:
     char mem[32758] = { 0 };
     string programName; // 프로그램 이름
     size_t programStart = 0; // 프로그램 논리적 시작 주소
     size_t loadAddress = 0; // 프로그램이 실제로 로드되는 주소
     size_t programLength = 0; // 프로그램 길이
+
+public:
+    /** 메모리 read/write */
+    void writeByte(size_t address, unsigned char value) { mem[address] = value; }
+    unsigned char readByte(size_t address) const { return mem[address]; }
+
+    /** 메모리 관련 */
+    char* getMemPtr() { return mem; }
+    size_t getMemSize() const { return sizeof(mem); }
+
+    /** getter / setter */
+    void setProgramName(const string &name) { programName = name; }
+    string getProgramName() const { return programName; }
+
+    void setProgramStart(size_t val) { programStart = val; }
+    size_t getProgramStart() const { return programStart; }
+
+    void setLoadAddress(size_t val) { loadAddress = val; }
+    size_t getLoadAddress() const { return loadAddress; }
+
+    void setProgramLength(size_t val) { programLength = val; }
+    size_t getProgramLength() const { return programLength; }
 };
 
 /**
@@ -101,11 +127,11 @@ void HParse(string record, Memory &memory) {
         hRecord[i] = trim(hRecord[i]);
     }
 
-    memory.programName = hRecord[0].substr(1);
-    memory.programStart = hexstrToHex(hRecord[1].substr(0, 6));
-    memory.programLength = hexstrToHex(hRecord[1].substr(6, 6));
+    memory.setProgramName(hRecord[0].substr(1));
+    memory.setProgramStart(hexstrToHex(hRecord[1].substr(0, 6)));
+    memory.setProgramLength(hexstrToHex(hRecord[1].substr(6, 6)));
 
-    if (32758 - memory.loadAddress < memory.programLength) {
+    if (32758 - memory.getLoadAddress() < memory.getProgramLength()) {
         cout << "out of range" << endl;
         return;
     }
@@ -115,13 +141,13 @@ void HParse(string record, Memory &memory) {
  * T 레코드 읽기
  */
 void TParse(string record, Memory &memory) {
-    size_t offset = memory.loadAddress - memory.programStart; // 오프셋: 실제 로드되는 주소와 프로그램에 작성한 주소간 거리차
+    size_t offset = memory.getLoadAddress() - memory.getProgramStart(); // 오프셋: 실제 로드되는 주소와 프로그램에 작성한 주소간 거리차
     size_t address = hexstrToHex(record.substr(1,6)) + offset; // 메모리에 로드되는 주소
     size_t len = hexstrToHex(record.substr(7, 2));  // 길이
     string relocation = record.substr(9,3); // 재배치 비트
     string objcode = record.substr(12); // object code 부분
 
-    if (len == objcode.length()) { // 재배치 비트 방식 사용했다면
+    if (len == objcode.length() / 2) { // 재배치 비트 방식 사용했다면
         vector<char> relocBits; // 재배치 비트 (워드 단위)
         for (char r : relocation) {
             string rStr = hexstrToBin(r);
@@ -142,9 +168,9 @@ void TParse(string record, Memory &memory) {
             }
 
             // 3바이트씩 메모리에 저장 (상위 -> 중간 -> 하위)
-            memory.mem[address++] = (word >> 16) & 0xFF;
-            memory.mem[address++] = (word >> 8) & 0xFF;
-            memory.mem[address++] = word & 0xFF;
+            memory.writeByte(address++, (word >> 16) & 0xFF);
+            memory.writeByte(address++, (word >> 8) & 0xFF);
+            memory.writeByte(address++, word & 0xFF);
 
             index += 6;
             wordCount++;
@@ -153,7 +179,7 @@ void TParse(string record, Memory &memory) {
         // 남은 바이트 처리 (워드 3바이트 미만)
         while (index + 2 <= objcode.length()) {
             unsigned char c = (unsigned char)hexstrToHex(objcode.substr(index,2));
-            memory.mem[address++] = c;
+            memory.writeByte(address++, c);
             index += 2;
         }
     } else { // 재배치 비트 사용하지 않았다면
@@ -163,7 +189,7 @@ void TParse(string record, Memory &memory) {
         
         while (index + 2 <= objcode.length()) { // 바이트 단위로 로드
             unsigned char c = (unsigned char)hexstrToHex(objcode.substr(index,2));
-            memory.mem[address++] = c;
+            memory.writeByte(address++, c);
             index += 2;
         }
     }
@@ -173,7 +199,7 @@ void TParse(string record, Memory &memory) {
  * M 레코드 읽기
  */
 void MParse(string record, Memory &memory) {
-    size_t offset = memory.loadAddress - memory.programStart; // 오프셋: 실제 로드되는 주소와 프로그램에 작성한 주소간 거리차
+    size_t offset = memory.getLoadAddress() - memory.getProgramStart(); // 오프셋: 실제 로드되는 주소와 프로그램에 작성한 주소간 거리차
     size_t address = hexstrToHex(record.substr(1,6)) + offset; // 메모리에 로드되는 주소
     size_t nibbles = hexstrToHex(record.substr(7, 2));
     string firstHalfNibble = record.substr(7, 1);
@@ -184,7 +210,7 @@ void MParse(string record, Memory &memory) {
     // 수정
     for (int i = 0; i < count; i++) { // 홀수로 들어와도 처리하기 위해 nibbles + 1
         stringstream ss;
-        ss << setw(2) << setfill('0') << hexToHexstr((unsigned char)memory.mem[address + i]);
+        ss << setw(2) << setfill('0') << hexToHexstr((unsigned char)memory.readByte(address + i));
         string tmp = ss.str();
         targetStr += tmp;
     }
@@ -193,13 +219,13 @@ void MParse(string record, Memory &memory) {
     
     // 메모리에 값 할당
     for (int i = 0; i < (count); i++) {
-        memory.mem[address + i] = (target >> 8*(count-i-1)) & 0xFF;
+        memory.writeByte(address + i, (target >> 8*(count-i-1)) & 0xFF);
     }
 
     // 수정 시작 주소의 값은 하프 니블 수정
-    string secondHalfNibble = hexToHexstr((unsigned char)memory.mem[address]).substr(0);
+    string secondHalfNibble = hexToHexstr((unsigned char)memory.readByte(address)).substr(0);
     string resultNibble = firstHalfNibble + secondHalfNibble;
-    memory.mem[address] = hexstrToHex(resultNibble);
+    memory.writeByte(address, hexstrToHex(resultNibble));
 }
 
 /**
@@ -212,7 +238,7 @@ void EParse(string record, Memory &memory) {
 void printMemory(Memory &memory) {       
     ofstream memoryf("Memory State.txt");
     
-    for (int i = 0; i < sizeof(memory.mem); i++) {
+    for (int i = 0; i < memory.getMemSize(); i++) {
         if (i % 8 == 0) {
             memoryf << "\n"
                  << uppercase
@@ -228,17 +254,17 @@ void printMemory(Memory &memory) {
         }
         memoryf << uppercase
              << hex << setw(2) << setfill('0') 
-             << (int)(unsigned char)memory.mem[i] << " ";
+             << (int)(unsigned char)memory.readByte(i) << " ";
 
         cout << uppercase
              << hex << setw(2) << setfill('0') 
-             << (int)(unsigned char)memory.mem[i] << " ";
+             << (int)(unsigned char)memory.readByte(i) << " ";
     }
 }
 
 int main() {
     Memory memory;
-    memory.programStart = 0;
+    memory.setProgramStart(0);
     string file;
     string inputStart;
     
@@ -247,7 +273,7 @@ int main() {
 
     cout << "프로그램 시작 주소 입력: ";
     cin >> inputStart;
-    memory.loadAddress = hexstrToHex(inputStart);
+    memory.setLoadAddress(hexstrToHex(inputStart));
 
     fileRead(file, memory);
 
